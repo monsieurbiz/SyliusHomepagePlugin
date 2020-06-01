0bd5839095a41401edfe55556662fb1a1d2b45a8
Maxime Huran <m.huran@monsieurbiz.com>
2020-06-01 15:36:38 +0200

    Manage entry in menu

---
+++ src/Menu/AdminMenuListener.php
@@ -0,0 +1,44 @@
+<?php
+declare(strict_types=1);
+
+namespace MonsieurBiz\SyliusHomepagePlugin\Menu;
+
+use Sylius\Bundle\UiBundle\Menu\Event\MenuBuilderEvent;
+use Knp\Menu\Util\MenuManipulator;
+
+final class AdminMenuListener
+{
+    /**
+     * @var MenuManipulator
+     */
+    private MenuManipulator $manipulator;
+
+    /**
+     * AdminMenuListener constructor.
+     *
+     * @param MenuManipulator $manipulator
+     */
+    public function __construct(MenuManipulator $manipulator)
+    {
+        $this->manipulator = $manipulator;
+    }
+
+    public function addAdminMenuItem(MenuBuilderEvent $event): void
+    {
+        $menu = $event->getMenu();
+
+        if (!$content = $menu->getChild('mbiz-cms')) {
+            $content = $menu
+                ->addChild('mbiz-cms')
+                ->setLabel('monsieurbiz_homepage.ui.cms_content')
+            ;
+        }
+
+        $content->addChild('mbiz-homepage-homepage', ['route' => 'monsieurbiz_homepage_admin_homepage_index'])
+            ->setLabel('monsieurbiz_homepage.ui.homepages')
+            ->setLabelAttribute('icon', 'home')
+        ;
+        $this->manipulator->moveChildToPosition($menu, $content, 0);
+    }
+
+}

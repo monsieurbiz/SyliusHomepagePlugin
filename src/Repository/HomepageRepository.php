0ddd5d8ae0ccaedfe4059bd95e64c07c850a5a35
Maxime Huran <m.huran@monsieurbiz.com>
2020-06-01 17:45:21 +0200

    Add customer repository for homepage

---
+++ src/Repository/HomepageRepository.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MonsieurBiz\SyliusHomepagePlugin\Repository;
+
+use Doctrine\ORM\QueryBuilder;
+use MonsieurBiz\SyliusHomepagePlugin\Entity\HomePage\Homepage;
+use Sylius\Bundle\ResourceBundle\Doctrine\ORM\EntityRepository;
+use Sylius\Component\Core\Model\ChannelInterface;
+
+class HomepageRepository extends EntityRepository
+{
+    /**
+     * @param string $localeCode
+     * @return QueryBuilder
+     */
+    public function createListQueryBuilder(string $localeCode): QueryBuilder
+    {
+        return $this->createQueryBuilder('o')
+            ->addSelect('translation')
+            ->leftJoin('o.translations', 'translation', 'WITH', 'translation.locale = :localeCode')
+            ->setParameter('localeCode', $localeCode)
+        ;
+    }
+
+    public function findOneByChannelAndLocale(ChannelInterface $channel, string $locale): ?Homepage
+    {
+        return $this->createQueryBuilder('o')
+            ->addSelect('translation')
+            ->innerJoin('o.translations', 'translation', 'WITH', 'translation.locale = :locale')
+            ->andWhere(':channel MEMBER OF o.channels')
+            ->addOrderBy('o.createdAt', 'DESC')
+            ->setParameter('channel', $channel)
+            ->setParameter('locale', $locale)
+            ->getQuery()
+            ->getOneOrNullResult()
+            ;
+    }
+}

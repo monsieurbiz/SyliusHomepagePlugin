e45cc7b5b9c83d2d08893a18421e6eeb43f23cfb
Maxime Huran <m.huran@monsieurbiz.com>
2020-06-01 15:35:03 +0200

    Define plugin and dependency injection

---
+++ src/DependencyInjection/MonsieurBizSyliusHomepageExtension.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MonsieurBiz\SyliusHomepagePlugin\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+final class MonsieurBizSyliusHomepageExtension extends Extension
+{
+
+    CONST EXTENSION_CONFIG_NAME = 'monsieur_biz_sylius_sales_reports';
+
+    public function load(array $config, ContainerBuilder $container)
+    {
+        $config = $this->processConfiguration($this->getConfiguration([], $container), $config);
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services.yaml');
+    }
+}

326ee3c393f06b959934fed3839e4cdba6b0ec09
Maxime Huran <m.huran@monsieurbiz.com>
2020-11-14 11:21:38 +0100

    Add getContainerExtension in MonsieurBizSyliusHomepagePlugin

--- src/MonsieurBizSyliusHomepagePlugin.php
+++ src/MonsieurBizSyliusHomepagePlugin.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace MonsieurBiz\SyliusHomepagePlugin;
 
+use LogicException;
 use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
 use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
@@ -11,4 +12,26 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 final class MonsieurBizSyliusHomepagePlugin extends Bundle
 {
     use SyliusPluginTrait;
+
+    /**
+     * Returns the plugin's container extension.
+     *
+     * @throws LogicException
+     *
+     * @return ExtensionInterface|null The container extension
+     */
+    public function getContainerExtension(): ?ExtensionInterface
+    {
+        if (null === $this->containerExtension) {
+            $this->containerExtension = false;
+            $extension = $this->createContainerExtension();
+            if (null !== $extension) {
+                $this->containerExtension = $extension;
+            }
+        }
+
+        return $this->containerExtension instanceof ExtensionInterface
+            ? $this->containerExtension
+            : null;
+    }
 }

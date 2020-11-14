436dfe7aa875b6d79657b03faea0c6842e1d3db4
Maxime Huran <m.huran@monsieurbiz.com>
2020-11-14 11:11:32 +0100

    Add Entities interfaces

---
+++ src/Entity/Homepage/HomepageInterface.php
@@ -0,0 +1,95 @@
+<?php
+declare(strict_types=1);
+
+namespace MonsieurBiz\SyliusHomepagePlugin\Entity\Homepage;
+
+use Doctrine\Common\Collections\Collection;
+use Gedmo\Timestampable\Timestampable;
+use Sylius\Component\Channel\Model\ChannelInterface;
+use Sylius\Component\Resource\Model\ResourceInterface;
+use Sylius\Component\Resource\Model\TimestampableInterface;
+use Sylius\Component\Resource\Model\TranslatableInterface;
+
+interface HomepageInterface extends ResourceInterface, TranslatableInterface, TimestampableInterface, Timestampable
+{
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int;
+
+    /**
+     * @return string|null
+     */
+    public function getName(): ?string;
+
+    /**
+     * @param string|null $name
+     * @return void
+     */
+    public function setName(?string $name): void;
+
+    /**
+     * @return string|null
+     */
+    public function getContent(): ?string;
+
+    /**
+     * @param string|null $content
+     * @return void
+     */
+    public function setContent(?string $content): void;
+
+    /**
+     * @return string|null
+     */
+    public function getMetaTitle(): ?string;
+
+    /**
+     * @param string|null $metaTitle
+     * @return void
+     */
+    public function setMetaTitle(?string $metaTitle): void;
+
+    /**
+     * @return string|null
+     */
+    public function getMetaDescription(): ?string;
+
+    /**
+     * @param string|null $metaDescription
+     * @return void
+     */
+    public function setMetaDescription(?string $metaDescription): void;
+
+    /**
+     * @return string|null
+     */
+    public function getMetaKeywords(): ?string;
+
+    /**
+     * @param string|null $metaKeywords
+     * @return void
+     */
+    public function setMetaKeywords(?string $metaKeywords): void;
+
+    /**
+     * @return Collection
+     */
+    public function getChannels(): Collection;
+
+    /**
+     * @param ChannelInterface $channel
+     */
+    public function addChannel(ChannelInterface $channel): void;
+
+    /**
+     * @param ChannelInterface $channel
+     */
+    public function removeChannel(ChannelInterface $channel): void;
+
+    /**
+     * @param ChannelInterface $channel
+     * @return bool
+     */
+    public function hasChannel(ChannelInterface $channel): bool;
+}

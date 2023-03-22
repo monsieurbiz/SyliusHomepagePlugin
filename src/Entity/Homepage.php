8f482af827ba805f751c4f2033a3442eb5d8584b
Etienne Gutbub <e.gutbub@monsieurbiz.com>
2023-03-22 16:37:27 +0100

    feat: Allow Homepage entities to be overriden + fix homepage grid when overriding an entity

--- src/Entity/Homepage.php
+++ src/Entity/Homepage.php
@@ -5,7 +5,7 @@
  *
  * (c) Monsieur Biz <sylius@monsieurbiz.com>
  *
- * For the full copyright and license information, please view the LICENSE.txt
+ * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
 
@@ -16,59 +16,38 @@ namespace MonsieurBiz\SyliusHomepagePlugin\Entity;
 use DateTimeInterface;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
-use Doctrine\ORM\Mapping as ORM;
-use Gedmo\Mapping\Annotation as Gedmo;
 use Sylius\Component\Channel\Model\ChannelInterface;
 use Sylius\Component\Resource\Model\TimestampableTrait;
 use Sylius\Component\Resource\Model\TranslatableTrait;
 use Sylius\Component\Resource\Model\TranslationInterface;
 use Webmozart\Assert\Assert;
 
-/**
- * @ORM\Entity
- * @ORM\Table(name="monsieurbiz_homepage_homepage")
- */
 class Homepage implements HomepageInterface
 {
     use TimestampableTrait;
-
     use TranslatableTrait {
         __construct as private initializeTranslationsCollection;
-
         getTranslation as private doGetTranslation;
     }
 
     /**
      * @var int|null
      *
-     * @ORM\Id
-     * @ORM\GeneratedValue
-     * @ORM\Column(type="integer")
      */
     private $id;
 
     /**
      * @var Collection<int, ChannelInterface>
-     * @ORM\ManyToMany(targetEntity="\Sylius\Component\Channel\Model\Channel")
-     * @ORM\JoinTable(
-     *     name="monsieurbiz_homepage_homepage_channels",
-     *     joinColumns={@ORM\JoinColumn(name="page_id", referencedColumnName="id")},
-     *     inverseJoinColumns={@ORM\JoinColumn(name="channel_id", referencedColumnName="id", unique=true)}
-     * )
      */
-    private $channels;
+    protected $channels;
 
     /**
      * @var DateTimeInterface|null
-     * @ORM\Column(name="created_at", type="datetime_immutable")
-     * @Gedmo\Timestampable(on="create")
      */
     protected $createdAt;
 
     /**
      * @var DateTimeInterface|null
-     * @ORM\Column(name="updated_at", type="datetime")
-     * @Gedmo\Timestampable(on="update")
      */
     protected $updatedAt;
 
@@ -81,6 +60,9 @@ class Homepage implements HomepageInterface
         $this->initializeChannelsCollection();
     }
 
+    /**
+     * @return int|null
+     */
     public function getId(): ?int
     {
         return $this->id;
@@ -94,11 +76,17 @@ class Homepage implements HomepageInterface
         return $this->channels;
     }
 
+    /**
+     * @param ChannelInterface $channel
+     */
     public function addChannel(ChannelInterface $channel): void
     {
         $this->channels->add($channel);
     }
 
+    /**
+     * @param ChannelInterface $channel
+     */
     public function removeChannel(ChannelInterface $channel): void
     {
         $this->channels->removeElement($channel);
@@ -109,63 +97,108 @@ class Homepage implements HomepageInterface
         $this->channels = new ArrayCollection();
     }
 
+    /**
+     * @param ChannelInterface $channel
+     *
+     * @return bool
+     */
     public function hasChannel(ChannelInterface $channel): bool
     {
         return $this->channels->contains($channel);
     }
 
+    /**
+     * @return string|null
+     */
     public function getName(): ?string
     {
         return $this->getTranslation()->getName();
     }
 
+    /**
+     * @param string|null $name
+     *
+     * @return void
+     */
     public function setName(?string $name): void
     {
         $this->getTranslation()->setName($name);
     }
 
+    /**
+     * @return string|null
+     */
     public function getContent(): ?string
     {
         return $this->getTranslation()->getContent();
     }
 
+    /**
+     * @param string|null $content
+     *
+     * @return void
+     */
     public function setContent(?string $content): void
     {
         $this->getTranslation()->setContent($content);
     }
 
+    /**
+     * @return string|null
+     */
     public function getMetaTitle(): ?string
     {
         return $this->getTranslation()->getMetaTitle();
     }
 
+    /**
+     * @param string|null $metaTitle
+     *
+     * @return void
+     */
     public function setMetaTitle(?string $metaTitle): void
     {
         $this->getTranslation()->setMetaTitle($metaTitle);
     }
 
+    /**
+     * @return string|null
+     */
     public function getMetaDescription(): ?string
     {
         return $this->getTranslation()->getMetaDescription();
     }
 
+    /**
+     * @param string|null $metaDescription
+     *
+     * @return void
+     */
     public function setMetaDescription(?string $metaDescription): void
     {
         $this->getTranslation()->setMetaDescription($metaDescription);
     }
 
+    /**
+     * @return string|null
+     */
     public function getMetaKeywords(): ?string
     {
         return $this->getTranslation()->getMetaKeywords();
     }
 
+    /**
+     * @param string|null $metaKeywords
+     *
+     * @return void
+     */
     public function setMetaKeywords(?string $metaKeywords): void
     {
         $this->getTranslation()->setMetaKeywords($metaKeywords);
     }
 
     /**
-     * @inheritdoc
+     * {@inheritdoc}
      */
     protected function createTranslation(): HomepageTranslation
     {
@@ -173,6 +206,8 @@ class Homepage implements HomepageInterface
     }
 
     /**
+     * @param string|null $locale
+     *
      * @return HomepageTranslationInterface
      */
     public function getTranslation(?string $locale = null): TranslationInterface
--- src/Entity/HomepageTranslation.php
+++ src/Entity/HomepageTranslation.php
@@ -5,7 +5,7 @@
  *
  * (c) Monsieur Biz <sylius@monsieurbiz.com>
  *
- * For the full copyright and license information, please view the LICENSE.txt
+ * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
 
@@ -16,53 +16,35 @@ namespace MonsieurBiz\SyliusHomepagePlugin\Entity;
 use Doctrine\ORM\Mapping as ORM;
 use Sylius\Component\Resource\Model\AbstractTranslation;
 
-/**
- * @ORM\Entity
- * @ORM\Table(name="monsieurbiz_homepage_homepage_translation")
- */
 class HomepageTranslation extends AbstractTranslation implements HomepageTranslationInterface
 {
     /**
      * @var int
-     *
-     * @ORM\Id
-     * @ORM\GeneratedValue
-     * @ORM\Column(type="integer")
      */
     protected $id;
 
     /**
      * @var string|null
-     *
-     * @ORM\Column(type="string", length=255, nullable=true)
      */
     protected $name;
 
     /**
      * @var string|null
-     *
-     * @ORM\Column(type="text", nullable=true)
      */
     protected $content;
 
     /**
      * @var string|null
-     *
-     * @ORM\Column(type="string", length=255, nullable=true)
      */
     protected $metaTitle;
 
     /**
      * @var string|null
-     *
-     * @ORM\Column(type="string", length=255, nullable=true)
      */
     protected $metaKeywords;
 
     /**
      * @var string|null
-     *
-     * @ORM\Column(type="text", nullable=true)
      */
     protected $metaDescription;
 
@@ -70,61 +52,100 @@ class HomepageTranslation extends AbstractTranslation implements HomepageTransla
      * Redeclared for annotations.
      *
      * @var string|null
-     *
-     * @ORM\Column(type="string", length=10, nullable=true)
      */
     protected $locale;
 
+    /**
+     * @return int|null
+     */
     public function getId(): ?int
     {
         return $this->id;
     }
 
+    /**
+     * @return string|null
+     */
     public function getName(): ?string
     {
         return $this->name;
     }
 
+    /**
+     * @param string|null $name
+     */
     public function setName(?string $name): void
     {
         $this->name = $name;
     }
 
+    /**
+     * @return string|null
+     */
     public function getContent(): ?string
     {
         return $this->content;
     }
 
+    /**
+     * @param string|null $content
+     *
+     * @return void
+     */
     public function setContent(?string $content): void
     {
         $this->content = $content;
     }
 
+    /**
+     * @return string|null
+     */
     public function getMetaTitle(): ?string
     {
         return $this->metaTitle;
     }
 
+    /**
+     * @param string|null $metaTitle
+     *
+     * @return void
+     */
     public function setMetaTitle(?string $metaTitle): void
     {
         $this->metaTitle = $metaTitle;
     }
 
+    /**
+     * @return string|null
+     */
     public function getMetaDescription(): ?string
     {
         return $this->metaDescription;
     }
 
+    /**
+     * @param string|null $metaDescription
+     *
+     * @return void
+     */
     public function setMetaDescription(?string $metaDescription): void
     {
         $this->metaDescription = $metaDescription;
     }
 
+    /**
+     * @return string|null
+     */
     public function getMetaKeywords(): ?string
     {
         return $this->metaKeywords;
     }
 
+    /**
+     * @param string|null $metaKeywords
+     *
+     * @return void
+     */
     public function setMetaKeywords(?string $metaKeywords): void
     {
         $this->metaKeywords = $metaKeywords;
